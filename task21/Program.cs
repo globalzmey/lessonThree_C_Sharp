9f558ad5b24fc66eb7cc2061bc7a2d7999b15622
Zmey <global.zmey@gmail.com>
2023-04-06 16:33:27 +0300

    Completed HW

---
+++ task21/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+Задача 21
+Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+A (3,6,8); B (2,1,-7), -> 15.84
+A (7,-5, 0); B (1,-1,9) -> 11.53
+*/
+
+int x1 = CoordFill("x", "A");
+int y1 = CoordFill("y", "A");
+int z1 = CoordFill("z", "A");
+int x2 = CoordFill("x", "B");
+int y2 = CoordFill("y", "B");
+int z2 = CoordFill("z", "B");
+
+int CoordFill(string coordName, string pointName)
+{
+    Console.Write($"Введите координату {coordName} точки {pointName}: ");
+    return Convert.ToInt16(Console.ReadLine());
+}
+
+double strSegLen(double x1, double x2, double y1, double y2, double z1, double z2)
+{
+    return Math.Sqrt(Math.Pow((x2-x1), 2) + Math.Pow((y2-y1), 2) + Math.Pow((z2-z1), 2));
+}
+
+Console.WriteLine($"Длина отрезка: {Math.Round(strSegLen(x1, x2, y1, y2, z1, z2), 2 )}");
---
+++ task23/Program.cs
@@ -0,0 +1,33 @@
+﻿/*
+Задача 23
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125
+*/
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+void toFill(int[] cubes){
+  int counter = 1;
+  int length = cubes.Length;
+  while (counter < length){
+    cubes[counter] = Convert.ToInt32(Math.Pow(counter, 3));
+    counter++;
+  }
+}
+
+void PrintCubes(int[] quantity){
+  int count = quantity.Length;
+  int index = 1;
+  while(index < count){
+    Console.Write(quantity[index]+ " ");
+    index++;
+  }
+} 
+
+int[] arry = new int[num + 1];
+toFill(arry);
+Console.Write(num + " -> ");
+PrintCubes(arry);
+Console.WriteLine();
